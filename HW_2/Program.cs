063051c436369fdce116aa530ec01fac1b549c8e
K0N5T4NT1N <vanburengen@gmail.com>
2023-02-05 22:19:00 +0300

    HW_2_Q13

--- HW_2/Program.cs
+++ HW_2/Program.cs
@@ -8,12 +8,12 @@
 */
 
 Console.Clear();
-Console.WriteLine("Задача № 10 определение второго числа");
+Console.WriteLine("Задача 10 определение второго числа");
 Console.Write("Введите трехзначное число: ");
 int num;
-if (! int.TryParse(Console.ReadLine(), out num))
+if (!int.TryParse(Console.ReadLine(), out num))
 {
-  System.Console.WriteLine("Некорректный ввод");
+  Console.WriteLine("Некорректный ввод");
   return;
 }
 
@@ -30,11 +30,36 @@ Console.ReadKey();
 Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
 645 -> 5
-
 78 -> третьей цифры нет
-
 32679 -> 6
+*/
 
+Console.Clear();
+Console.WriteLine("Задача 13 определение третьей цифры числа");
+Console.Write("Введите число: ");
+int Numbers;
+if (!int.TryParse(Console.ReadLine(), out Numbers))
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+int Value = Math.Abs(Numbers);   // Проверка отрицательных чисел
+
+if (Value <= 99)
+{
+    Console.WriteLine("Число не является трехзначным");
+    return;
+}
+
+while (Value > 999)
+{
+    if (Value > 999) Value /= 10;
+}
+
+Console.WriteLine($"Третья цифра в числе - {Value % 10}");
+
+/*
 Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 
 6 -> да
