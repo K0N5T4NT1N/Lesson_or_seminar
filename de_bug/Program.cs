9f1ce1c5d424d874d85f6f87390274be2132317e
K0N5T4NT1N <123107372+K0N5T4NT1N@users.noreply.github.com>
2023-02-12 22:23:37 +0300

    New_HV

---
+++ de_bug/Program.cs
@@ -0,0 +1,49 @@
+﻿//
+#nullable disable
+Console.Clear();
+Console.WriteLine("Задача 19: определение является ли число полиндромом"); //альтернитивное решение
+Console.WriteLine("Введите целое число:");
+long NumPoly;
+if (!long.TryParse(Console.ReadLine(), out NumPoly))
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+long NumPolyModul = Math.Abs(NumPoly);
+int Depth = -1;
+long ValueTemp = NumPolyModul;
+long ValueBegin = 0;
+long ValueEnd = 0;
+long ValueInvert = 0;
+
+for (; ValueTemp > 0; Depth++)
+{
+    ValueTemp /= 10;
+}
+ValueTemp = NumPolyModul; 
+// Console.WriteLine(Depth);
+// Console.ReadKey();
+ValueBegin = (long)Math.Pow(10, Depth);
+// Console.WriteLine(ValueBegin);
+// Console.ReadKey();
+while (Depth >= 0)
+{
+    ValueEnd = ValueTemp % 10;
+    ValueTemp /= 10;
+    ValueEnd *= ValueBegin;
+    ValueBegin /= 10;
+    ValueInvert = ValueInvert + ValueEnd;
+    Depth--;
+}
+// Console.WriteLine(ValueInvert);
+// Console.ReadKey();
+    if (ValueInvert == NumPolyModul)
+{
+    Console.WriteLine($"{NumPoly} -> да, полиндром");
+}
+    else
+{
+    Console.WriteLine($"{NumPoly} -> не полиндром");
+}
+Console.WriteLine("Для продолжения нажмите любую клавишу..." );
+Console.ReadKey();
