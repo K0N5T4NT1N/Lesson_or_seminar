934d8b4b2afe9d1b39ca717ae34bf78ef970d9d0
K0N5T4NT1N <123107372+K0N5T4NT1N@users.noreply.github.com>
2023-02-13 19:01:03 +0300

    hw_5_init

--- HW_4/Program.cs
+++ HW_4/Program.cs
@@ -28,6 +28,7 @@ int[] randomArray(int length, int minValue, int maxValue)
     return result;
 }
 
+#nullable disable
 
 /* Задача 25: Напишите цикл, который принимает на вход 
 два числа (A и B) и возводит число A в натуральную степень B.
