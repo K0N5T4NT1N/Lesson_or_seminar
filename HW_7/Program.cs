828bb3431b5da3709efe3a41340cc6c2e6df564a
K0N5T4NT1N <123107372+K0N5T4NT1N@users.noreply.github.com>
2023-02-22 21:46:16 +0300

    Task_47

--- HW_7/Program.cs
+++ HW_7/Program.cs
@@ -1,2 +1,152 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+
+Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+
+#nullable disable
+//методы начало
+int InputUser(string message)
+{
+    Console.Write(message);
+    string ReadVoid = Console.ReadLine();
+    var result = int.Parse(ReadVoid);
+    return result;
+}
+
+double InputUserDouble(string message)
+{
+    Console.Write(message);
+    string ReadVoid = Console.ReadLine();
+    var result = Convert.ToDouble(ReadVoid);
+    return result;
+}
+
+int[,] GetMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int l = 0; l < columns; l++)
+        {
+            matrix[i, l] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void Print(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int l = 0; l < matrix.GetLength(1); l++)
+        {
+            Console.Write(matrix[i, l] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+//методы конец
+
+Console.Clear();
+
+HomeWork();
+
+void HomeWork()
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Задача 47: Создание двумерного массива размером m×n, заполненного случайными вещественными числами.\n");
+        Console.WriteLine("Задача 50: Проверка позиции заданного элемента в двумерном массиве. (Возвращает значение этого элемента либо указывает, что такого элемента нет).\n");
+        Console.WriteLine("Задача 52: Нахождение среднго арифметического элементов в каждом столбце заданного массива. \n");
+        Console.WriteLine("Задача 62: Заполнение спирально массива 4 на 4. \n");
+        Console.WriteLine("Для выхода введите: 0 \n");
+
+        int numTask = InputUser("Введите номер задачи: ");
+
+        switch (numTask)
+        {
+            case 0: return;
+
+            case 41:
+                Console.Clear();
+                Console.Write("Какое количество чисел проверить? ");
+                int elementsCount;
+                if (!int.TryParse(Console.ReadLine(), out elementsCount) || elementsCount <= 0)
+                {
+                    Console.WriteLine("Некорректный ввод");
+                    return;
+                }
+                int[] array41 = new int[elementsCount];
+                for (var i = 0; i < elementsCount; i++)
+                {
+                    Console.Write($"\nВведите элемент массива с индексом {i}: ");
+                    array41[i] = int.Parse(Console.ReadLine());
+                }
+                OutputArray(array41); Console.WriteLine($" Количество чисел больше нуля -> {CountIndexPositiv(array41)}");
+                Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+                Console.ReadKey();
+                break;
+
+            case 43:
+                Console.Clear();
+                double b1 = InputUserDouble("введите значение b1 = ");
+
+                double k1 = InputUserDouble("введите число k1 = ");
+
+                double b2 = InputUserDouble("введите значение b2 = ");
+
+                double k2 = InputUserDouble("введите число k2 = ");
+
+                double x = (b2 - b1) / (k1 - k2);
+                double y = (k2 * x + b2);
+
+                if (y != k1 * x + b1 || k1 - k2 == 0)
+                {
+                    Console.WriteLine("Решений нет!");
+                }
+                else
+                {
+                    Console.WriteLine($"две прямые пересекутся в точке с координатами ( X: {x}, Y: {y} )");
+                }
+                Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+                Console.ReadKey();
+                break;
+
+            case 44:
+                int fibonacciN = InputUser("До какого числа показать ряд?: ");
+                Fibonacci(fibonacciN); Console.WriteLine("\nДля продолжения нажмите любую клавишу...");
+                Console.ReadKey();
+                break;
+
+            default: Console.WriteLine("Такой задачи не существует"); break;
+        }
+    }
+}
